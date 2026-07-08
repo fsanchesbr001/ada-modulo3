c366b6b39b3f5895438a7b2f9ebd2d102425069d
Fabricio A. S. Sanches <fsanchesbr001@gmail.com>
2026-07-08 01:13:01 -0300

    Initial commit

---
+++ src/main/java/com/fabriciosanches/adamodulo3/AdaModulo3Application.java
@@ -0,0 +1,13 @@
+package com.fabriciosanches.adamodulo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaModulo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaModulo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabriciosanches/adamodulo3/AdaModulo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciosanches.adamodulo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaModulo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
