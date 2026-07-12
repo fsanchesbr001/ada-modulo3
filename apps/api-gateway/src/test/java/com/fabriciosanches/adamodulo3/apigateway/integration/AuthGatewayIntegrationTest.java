6244ad2313d5c58c18a01124caf793929e846751
Fabricio A. S. Sanches <fsanchesbr001@gmail.com>
2026-07-12 03:13:03 -0300

    Correções

--- apps/api-gateway/src/test/java/com/fabriciosanches/adamodulo3/apigateway/integration/AuthGatewayIntegrationTest.java
+++ apps/api-gateway/src/test/java/com/fabriciosanches/adamodulo3/apigateway/integration/AuthGatewayIntegrationTest.java
@@ -1,10 +1,22 @@
 package com.fabriciosanches.adamodulo3.apigateway.integration;
 
+import com.fabriciosanches.adamodulo3.apigateway.application.LoginUseCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude="
+                + "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,"
+                + "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
+@ActiveProfiles("test")
 class AuthGatewayIntegrationTest {
+    @MockBean
+    private LoginUseCase loginUseCase;
+
     @Test
     void contextLoads() {
     }
